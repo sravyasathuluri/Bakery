cd1ec0f95c0c183b2fd3b6e1122d06cfae5f9aa1
Sathuluri, Sravya <sravya.sathuluri@accenture.com>
2025-11-19 17:08:43 +0530

    push

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,5 +12,5 @@ namespace Bakery.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageName { get; set; }        
-    }
+    
 }
