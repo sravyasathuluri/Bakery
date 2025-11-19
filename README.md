f2fb515fa4f114fd2b82be3e246b6b0b8bd1e77e
Sathuluri, Sravya <sravya.sathuluri@accenture.com>
2025-11-19 17:06:01 +0530

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Bakery
+## Purpose
+Pratice the ASP.NET Core Web Appliation programming skill including  
+razor page model, mvc flow, database migration and access through EFC,
+deployment on Ubuntu Nginx
+## Reference
+Major code and reference is from [Learn Razor Pages](https://www.learnrazorpages.com/razor-pages/tutorial/bakery) by Mike Bride.
+## Content
+Major modification and practice including:  
+1. Send email from Gmail with 2 factors authentication enabled account
+2. Deployment on Ubuntu Nginx with reverse proxy and ASP.NET core 7.0 runtimes
+3. Practice database migration and access through EFC for sqlite3
+4. Embed video clip in razor page
+5. Using Visual Studio 2022 for Mac to build and publish to folder
+
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Bakery
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+    }
+}
+
